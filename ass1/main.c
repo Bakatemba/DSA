d6a08a6999fdcb1d003caa7606eda4546bcbdfaf
Itazya <eugenemwesigwa860@gmail.com>
2025-03-27 12:51:51 +0300

    using arrays

--- ass1/main.c
+++ ass1/main.c
@@ -1,155 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Physics//
-void Physics(){
+void Compilationof_Results() {
+    int n;
+    printf("Please enter the number of subjects you would wish to enter marks for:");
+    printf("\n");
+    scanf("%d",&n);
+    int Marks [n][4]; // 3 subjects, 4 marks per subject
+    double sum, average;
+    char subjects_names[n][20]; // Array to store subject names
 
-    int ass, cw, mt, et;
-
-    printf("Enter the results for your assignment: ");
-    scanf("%d", &ass);
-    while (ass > 100 || ass < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your assignment: ");
-        scanf("%d", &ass);
+    // Input subject names
+    printf("Enter names of  subjects:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Subject %d: ", i + 1);
+        scanf("%s", subjects_names[i]);
     }
 
-    printf("Enter the results for your course work: ");
-    scanf("%d", &cw);
-    while (cw > 100 || cw < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your course work: ");
-        scanf("%d", &cw);
+    // Enter marks for each subject
+    for (int k = 0; k < n; k++) {
+        sum = 0; // Reset sum for each subject
+
+        printf("\nEnter marks for %s (Asg, CW, MID, EOT):\n", subjects_names[k]);
+        for (int j = 0; j < 4; j++) {
+                while(1){
+            scanf("%d", &Marks[k][j]);
+            if(Marks[k][j]>=0&&Marks[k][j]<=100){
+                break;
+            }else{
+             printf("Invalid Input The Mark is Supposed to be between 0 and 100\n Please re-enter the mark\n ");}
+
+
+        }
+         sum += Marks[k][j]; // Sum up the marks
+        }
+
+        average = sum / 4;
+        printf("\nMarks for %s:\n", subjects_names[k]);
+        printf("Asg\t CW\t MID\t EOT\n");
+        // Printing the marks;
+        for (int j = 0; j < 4; j++) {
+            printf("%d\t", Marks[k][j]);
+        }
+
+        printf("\nSum: %.0lf", sum);
+        printf("\nAverage: %.2lf\n", average);
     }
-
-    printf("Enter the results for your midterms: ");
-    scanf("%d", &mt);
-    while (mt > 100 || mt < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your midterms: ");
-        scanf("%d", &mt);
-    }
-
-    printf("Enter the results for your end of term: ");
-    scanf("%d", &et);
-    while (et > 100 || et < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your end of term: ");
-        scanf("%d", &et);
-    }
-
-    int sum = et + mt + cw + ass;
-    float avg = sum / 4.0;
-
-    printf("Total marks: %d\nAverage marks: %.2f\n", sum, avg);
-}
-//CHEMISTRY//
-void Chemistry(){
-     int ass, cw, mt, et;
-
-    printf("Enter the results for your assignment: ");
-    scanf("%d", &ass);
-    while (ass > 100 || ass < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your assignment: ");
-        scanf("%d", &ass);
-    }
-
-    printf("Enter the results for your course work: ");
-    scanf("%d", &cw);
-    while (cw > 100 || cw < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your course work: ");
-        scanf("%d", &cw);
-    }
-
-    printf("Enter the results for your midterms: ");
-    scanf("%d", &mt);
-    while (mt > 100 || mt < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your midterms: ");
-        scanf("%d", &mt);
-    }
-
-    printf("Enter the results for your end of term: ");
-    scanf("%d", &et);
-    while (et > 100 || et < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your end of term: ");
-        scanf("%d", &et);
-    }
-
-    int sum = et + mt + cw + ass;
-    float avg = sum / 4.0;
-
-    printf("Total marks: %d\nAverage marks: %.2f\n", sum, avg);
 }
 
-//MATH//
-void math(){
-
-      int ass, cw, mt, et;
-
-    printf("Enter the results for your assignment: ");
-    scanf("%d", &ass);
-    while (ass > 100 || ass < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your assignment: ");
-        scanf("%d", &ass);
-    }
-
-    printf("Enter the results for your course work: ");
-    scanf("%d", &cw);
-    while (cw > 100 || cw < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your course work: ");
-        scanf("%d", &cw);
-    }
-
-    printf("Enter the results for your midterms: ");
-    scanf("%d", &mt);
-    while (mt > 100 || mt < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your midterms: ");
-        scanf("%d", &mt);
-    }
-
-    printf("Enter the results for your end of term: ");
-    scanf("%d", &et);
-    while (et > 100 || et < 0) {
-        printf("Marks should be between 0 and 100!\n");
-        printf("Enter the results for your end of term: ");
-        scanf("%d", &et);
-    }
-
-    int sum = et + mt + cw + ass;
-    float avg = sum / 4.0;
-
-    printf("Total marks: %d\nAverage marks: %.2f\n", sum, avg);
-}
-
-int main()
-{
-    printf("Choose 1 for Physics\n");
-    printf("Choose 2 for Chemistry\n");
-    printf("Choose 3 for Math\n");
-
-    int Selection=0;
-    scanf("%d",&Selection);
-
-    switch (Selection){
-case 1:
-    Physics();
-    break;
-case 2:
-    Chemistry();
-    break;
-case 3:
-    math();
-    break;
-default:
-    printf("Invalid Input");
-    break;
-    }
+int main() {
+    Compilationof_Results();
     return 0;
 }
